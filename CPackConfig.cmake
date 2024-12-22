c77d6ff447347b15047ad441c40aced31e0f1731
AndrewQuijano <andrewquijano92@gmail.com>
2024-12-21 22:24:59 -0500

    Create debian and rpm package on releases

---
+++ CPackConfig.cmake
@@ -0,0 +1,11 @@
+# Used to dynamically set the package file name based on the generator
+foreach(generator ${CPACK_GENERATOR})
+    if("${generator}" STREQUAL "DEB")
+        set(CPACK_PACKAGE_FILE_NAME ${CPACK_DEBIAN_PACKAGE_FILE_NAME})
+    elseif("${generator}" STREQUAL "RPM")
+        set(CPACK_PACKAGE_FILE_NAME ${CPACK_RPM_PACKAGE_FILE_NAME})
+    elseif("${generator}" STREQUAL "DragNDrop")
+        set(CPACK_PACKAGE_FILE_NAME ${CPACK_DMG_PACKAGE_FILE_NAME})
+    endif()
+    message(STATUS "Generating package for ${generator} with file name ${CPACK_PACKAGE_FILE_NAME}")
+endforeach()
